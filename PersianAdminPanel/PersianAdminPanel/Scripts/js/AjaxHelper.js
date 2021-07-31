911d9c6117e346577034dfe676b0a31cb80a4da6
n.kh <23635655+n-khosh@users.noreply.github.com>
2021-08-01 00:56:23 +0430

    ajaxhelper added

---
+++ PersianAdminPanel/PersianAdminPanel/Scripts/js/AjaxHelper.js
@@ -0,0 +1,27 @@
+﻿function AjaxHelper(url, jsonData, tag) {
+
+    $.ajax({
+        type: "post",
+        url: url,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        data: jsonData,
+        cache: false,
+        beforeSend: function () {
+            //showAjaxLoadingModal(tag);
+        },
+        success: function (data) {
+            //hideAjaxLoadingModal(tag);
+            if (data.code == success) {
+                //todo
+            }
+            else {
+                //Func_ShowModal('توجه', data.message);
+            }
+        },
+        error: function (xhr, status, error) {
+            //hideAjaxLoadingModal(tag);
+            //Func_ShowModal('توجه', 'خطایی در سیستم رخ داد');
+        }
+    });
+}
